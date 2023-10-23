bb42cc137dc92c5a6c2d2a695b4ff3d4f2c86a0a
bill <bilaltahacs@gmail.com>
2023-10-22 22:40:51 -0400

    Init Project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,22 +8,6 @@ import { ThemeService } from '@core/services/theme.service';
 })
 export class AppComponent {
     title = 'webrtc-p2p';
-    minutes = 0;
-    gender = 'female';
-    fly = true;
-    logo = 'https://angular.io/assets/images/logos/angular/angular.png';
-    inc(i: number) {
-        this.minutes = Math.min(5, Math.max(0, this.minutes + i));
-    }
-    male() {
-        this.gender = 'male';
-    }
-    female() {
-        this.gender = 'female';
-    }
-    other() {
-        this.gender = 'other';
-    }
 
     constructor(private themeService: ThemeService) {}
 
